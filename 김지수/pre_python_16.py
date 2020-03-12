69a2fd4fedc9cf3bf6fed22c0c4d1c4eebd33259
d-jskim <d.unicornhybrid@gmail.com>
2020-03-12 21:13:22 +0900

    16번 퀴즈 정답

--- 김지수/pre_python_16.py
+++ 김지수/pre_python_16.py
@@ -1,1 +1,22 @@
 """16. 1부터 50 까지의 숫자 중에서 3의 배수를 공백으로 구분하여 출력하시오."""
+
+
+
+def multiplesOfThree():
+
+    list = []
+
+    for i in range(50):
+        number = i + 1
+        if number % 3 == 0:
+            list.append(str(number)) #parseStr for using str.join()
+    
+    return list
+
+listRes = multiplesOfThree()
+
+
+print(" ".join(listRes))
+
+
+            
