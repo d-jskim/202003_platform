86d51e507089a420ba7040f3407fe3d89e5a0ba5
d-jskim <d.unicornhybrid@gmail.com>
2020-03-12 19:19:17 +0900

    10번 퀴즈 정답

--- 김지수/pre_python_10.py
+++ 김지수/pre_python_10.py
@@ -1,3 +1,21 @@
 """10. 팩토리얼을 구하는 함수를 작성하시오.
 ex ) 5! = 5x4x3x2x1
   print(factoral(5)) -> 120 출력"""
+
+
+def factorial(no):
+
+    #initialization for multiplication
+    result = 1
+
+    for i in reversed(range(no)):
+        number = i + 1
+        result *= number
+        
+    return result
+
+
+inputNo = int(input("정수를 입력하시오: "))
+factorialRes = factorial(inputNo)
+
+print("factorial({}) -> {}".format(inputNo, factorialRes))
