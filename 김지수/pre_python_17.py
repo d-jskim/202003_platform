a61e1736977563fe6a1ee81dcdd24b2ace626fe3
d-jskim <d.unicornhybrid@gmail.com>
2020-03-12 23:44:05 +0900

    17번 문제 정답

--- 김지수/pre_python_17.py
+++ 김지수/pre_python_17.py
@@ -3,4 +3,19 @@
 
 # 2002 World Cup was held in S.Korea
 def yrs_WorldCup():
-    
+    before2002 = []
+    after2002 = []
+
+    for year in range(2002, 1988, -4):
+        before2002.append(year)
+   
+    for year in range(2002 + 4, 2060, +4):
+        after2002.append(year)
+
+    before2002.reverse()
+
+    yearsOfWC = before2002 + after2002
+    print(yearsOfWC)
+
+ 
+yrs_WorldCup()
