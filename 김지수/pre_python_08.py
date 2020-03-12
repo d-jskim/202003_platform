6f0d4dcdad02a3cc430ab33f3860a6e05aabc6fd
d-jskim <d.unicornhybrid@gmail.com>
2020-03-12 18:53:35 +0900

    8번 퀴즈 정답

--- 김지수/pre_python_08.py
+++ 김지수/pre_python_08.py
@@ -1,1 +1,13 @@
 """8. 정수를 입력했을 때 짝수인지 홀수인지 핀별하는 코드를 작성하시오"""
+
+
+def oddAndEven(no):
+
+    if no % 2 == 0:
+        print("{}은(는) {}입니다.".format(no, "짝수"))
+    
+    else:
+        print("{}은(는) {}입니다.".format(no, "홀수"))
+
+inputNo = int(input("정수를 입력하시오: "))
+oddAndEven(inputNo)
