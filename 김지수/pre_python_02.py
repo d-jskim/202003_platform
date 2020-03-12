57b2c30e37e3fe33160a9a8889919ab6a3990d76
d-jskim <d.unicornhybrid@gmail.com>
2020-03-12 20:36:27 +0900

    2번 퀴즈 정답

--- 김지수/pre_python_02.py
+++ 김지수/pre_python_02.py
@@ -1,4 +1,25 @@
 """"2.if문을 이용해 첫번째와 두번째 수, 연산기호를 입력하게 하여 계산값이 나오는 계산기를 만드시오"""
 
-"""if문의 조건이 뭘까요? 값이 두 개가 입력됐는지 물어보는 걸까요?"""
+input1 = float(input("숫자를 입력하시오: "))
+inputOp = input("연산자를 입력하시요(+, -, *, /): ")
+input2 = float(input("숫자를 한 번 더 입력하시오: "))
 
+
+def cal(operator, a, b):
+
+    if operator == "+":
+        return a + b
+    
+    elif operator == "-":
+        return a - b
+
+    elif operator == "*":
+        return a * b
+    
+    elif operator =="/":
+        return a / b
+    else:
+        print("계산할 수 없습니다. '+', '-', '*', '/' 중 하나만 선택해주세요.")
+        return 
+
+print("연산 결과: ", cal(inputOp, input1, input2))
