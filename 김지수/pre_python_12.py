db50a95ca56c450b109b134a3d8836a3862b4b6c
d-jskim <d.unicornhybrid@gmail.com>
2020-03-12 20:20:18 +0900

    12번 퀴즈 정답

--- 김지수/pre_python_12.py
+++ 김지수/pre_python_12.py
@@ -2,3 +2,12 @@
 ABCD 순으로 정렬하시오.
 
 namelist = ['Mary', 'Sams', 'Aimy', 'Tom', 'Michale', 'Bob', 'Kelly']"""
+
+
+namelist = ['Mary', 'Sams', 'Aimy', 'Tom', 'Michale', 'Bob', 'Kelly']
+
+sortedList = sorted(namelist)
+
+# no change in namelist
+#print(namelist)
+print(sortedList)
