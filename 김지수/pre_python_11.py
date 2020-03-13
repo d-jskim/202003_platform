6523c793d7c8361d4565291007e118b778c436a3
d-jskim <d.unicornhybrid@gmail.com>
2020-03-13 13:29:49 +0900

    11번 문제 정답

--- 김지수/pre_python_11.py
+++ 김지수/pre_python_11.py
@@ -3,12 +3,11 @@
 # prime numbers: 소수
 primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
 
-# a list for common factors
-commonFactors = []
-
-
 # a function for finding the Greatest Common Factor
 def findGCF(a, b):
+    
+    # a list for common factors
+    commonFactors = []
 
     for i in range(len(primeNumbers)): 
         divisor = primeNumbers[i]
@@ -31,7 +30,7 @@ def findGCF(a, b):
 input1 = int(input("2이상의 양수를 입력하시오: "))
 input2 = int(input("2이상의 양수를 한 번 더 입력하시오:"))
 resGCF = findGCF(input1, input2)
-print("{}와(과){}의 최대공약수: {}".format(input1, input2, resGCF))
+print("{}와(과) {}의 최대공약수: {}".format(input1, input2, resGCF))
 
 
         
