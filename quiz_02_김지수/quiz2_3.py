273adb59c5a0367a0729cb506ce05909cabb48fd
d-jskim <d.unicornhybrid@gmail.com>
2020-03-20 20:30:29 +0900

    commit

--- quiz_02_김지수/quiz2_3.py
+++ quiz_02_김지수/quiz2_3.py
@@ -40,10 +40,10 @@ class Card:
     def consume(self, payment, place):
 
         if self.balance >= payment:
-            if place == "마트":
+            if place == '마트':
                 self.balance -= payment
                 print("마트에서 {}원 사용했습니다.".format(payment))
-            elif place == "영화관":
+            elif place == '영화관':
                 self.balance -= payment * 0.8
                 print("마트에서 {}원 사용했습니다.".format(payment))
             else: print("'마트' 또는 '영화관'을 정확하게 입력해 주십시오.")
@@ -57,6 +57,6 @@ class Card:
 card = Card()
 card.charge(20000)
 card.consume(3000,'마트')
-card.consume(10000,'영화관')
-card.consume(13000,'마트')
-card.print()
+# card.consume(10000,'영화관')
+# card.consume(13000,'마트')
+# card.print()
