b2391b8d335f56913e52a8eb20b0e0e6bc809a04
d-jskim <d.unicornhybrid@gmail.com>
2020-03-20 19:33:32 +0900

    퀴즈2_2번

--- quiz_02_김지수/quiz2_2.py
+++ quiz_02_김지수/quiz2_2.py
@@ -14,3 +14,24 @@ print("%d년 %d월 %d일은 %s 입니다." % (myYear, myMonth, myDay, printDayOf
 2020년 3월 13일은 금요일 입니다.
 
 '''
+
+"""
+date.weekday()
+정수로 요일을 반환합니다. 월요일은 0이고 일요일은 6입니다. 예를 들어, date(2002, 12, 4).weekday() == 2, 수요일. 
+"""
+
+from datetime import date
+
+myYear = int(input("연도를 입력하시오"))
+myMonth = int(input("월을 입력하시오"))
+myDay = int(input("일을 입력하시오"))
+
+# 요일 출력
+def printDayOfTheWeek(myYear, myMonth, myDay):
+
+    weekdays = ["월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일"]
+    weekday = weekdays[date(myYear, myMonth, myDay).weekday()]
+    return weekday
+
+
+print("%d년 %d월 %d일은 %s 입니다." % (myYear, myMonth, myDay, printDayOfTheWeek(myYear, myMonth, myDay)))
