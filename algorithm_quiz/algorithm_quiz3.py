847ffd12b65b8ce876ae2b73521c602dd125aaad
d-jskim <d.unicornhybrid@gmail.com>
2020-03-22 12:57:44 +0900

    Update algorithm_quiz3.py

--- algorithm_quiz/algorithm_quiz3.py
+++ algorithm_quiz/algorithm_quiz3.py
@@ -10,3 +10,23 @@ print(bubble_sort(list))
 
 <출력>
 [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 16, 21]'''
+
+
+
+list=[4,3,2,1,8,7,5,10,11,16,21,6]
+
+print("정상적으로 실행합니다.")
+
+def bubble_sort(list):
+
+    for length in range(len(list), 0, -1):
+        for j in range(length-1):
+            if list[j] > list[j+1]:
+                list[j], list[j+1] = list[j+1], list[j]
+    
+    return list
+
+print(bubble_sort(list))
+
+
+        
